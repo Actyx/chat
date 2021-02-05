07e3aa607fc1cc68dc2fe74ec43f4bcff970dc0d
GibboK <gibbok.coding@gmail.com>
2021-02-05 16:14:53 +0100

    typo

--- app/src/ui/PondError/PondError.tsx
+++ app/src/ui/PondError/PondError.tsx
@@ -2,6 +2,6 @@ import { FC } from 'react';
 
 export const PondError: FC<{}> = () => {
   return (
-    <div>Pond is not loaded. Make sure you have an node running ActyxOS</div>
+    <div>Pond is not loaded. Make sure you have a node running ActyxOS</div>
   );
 };
