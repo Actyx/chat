6dc7da37abba698706f420c7a6a901b9ad6267b1
GibboK <gibbok.coding@gmail.com>
2021-02-18 15:23:14 +0100

    test getMessageContentEdited

--- app/src/business-logic/channel-fish/events.test.ts
+++ app/src/business-logic/channel-fish/events.test.ts
@@ -1,4 +1,4 @@
-import { getPublicMessageAdded } from './events';
+import { getMessageContentEdited, getPublicMessageAdded } from './events';
 
 describe('getPublicMessageAdded', () => {
   it('should return valid tags and event', () => {
@@ -28,6 +28,40 @@ describe('getPublicMessageAdded', () => {
         createdBy: 'user-1',
         channelId: 'channel-1',
         content: 'Hello world!',
+      },
+    };
+
+    expect(result).toMatchObject([expectedTags, expectedEvent]);
+  });
+});
+
+describe('getMessageContentEdited', () => {
+  it('should return valid tags and event', () => {
+    const result = getMessageContentEdited(
+      'message-1',
+      'channel-1',
+      'edited message',
+      'user-1'
+    );
+
+    const expectedTags = {
+      rawTags: [
+        'messages-catalog',
+        'channel',
+        'channel:channel-1',
+        'message',
+        'message:message-1',
+        'user',
+        'user:user-1',
+      ],
+    };
+
+    const expectedEvent = {
+      type: 'MessageContentEdited',
+      payload: {
+        messageId: 'message-1',
+        content: 'edited message',
+        editedBy: 'user-1',
       },
     };
 
