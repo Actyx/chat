ee9ab510648d1d5638ef5e9f084c6a1c4916a99d
GibboK <gibbok.coding@gmail.com>
2021-02-23 08:10:44 +0100

    categorize tests under at least 2 levels

--- app/src/business-logic/channel-catalog-fish/events.test.ts
+++ app/src/business-logic/channel-catalog-fish/events.test.ts
@@ -7,172 +7,174 @@ import {
   getChannelUnarchived,
 } from './events';
 
-describe('getChannelAdded', () => {
-  it('should return valid tags and event', () => {
-    const result = getChannelAdded(
-      'channel-1',
-      'user-1',
-      'marketing',
-      'all about marketing'
-    );
+describe('ChannelCatalogFish', () => {
+  describe('getChannelAdded', () => {
+    it('should return valid tags and event', () => {
+      const result = getChannelAdded(
+        'channel-1',
+        'user-1',
+        'marketing',
+        'all about marketing'
+      );
 
-    const expectedTags = {
-      rawTags: [
-        'channel-catalog',
-        'channel',
-        'channel:channel-1',
-        'user',
-        'user:user-1',
-      ],
-    };
+      const expectedTags = {
+        rawTags: [
+          'channel-catalog',
+          'channel',
+          'channel:channel-1',
+          'user',
+          'user:user-1',
+        ],
+      };
 
-    const expectedEvent = {
-      type: 'ChannelAdded',
-      payload: {
-        channelId: 'channel-1',
-        createdBy: 'user-1',
-        name: 'marketing',
-        description: 'all about marketing',
-      },
-    };
+      const expectedEvent = {
+        type: 'ChannelAdded',
+        payload: {
+          channelId: 'channel-1',
+          createdBy: 'user-1',
+          name: 'marketing',
+          description: 'all about marketing',
+        },
+      };
 
-    expect(result).toMatchObject([expectedTags, expectedEvent]);
+      expect(result).toMatchObject([expectedTags, expectedEvent]);
+    });
   });
-});
 
-describe('getChannelProfileEdited', () => {
-  it('should return valid tags and event', () => {
-    const result = getChannelProfileEdited(
-      'channel-1',
-      'user-1',
-      'online marketing',
-      'all about online marketing'
-    );
+  describe('getChannelProfileEdited', () => {
+    it('should return valid tags and event', () => {
+      const result = getChannelProfileEdited(
+        'channel-1',
+        'user-1',
+        'online marketing',
+        'all about online marketing'
+      );
 
-    const expectedTags = {
-      rawTags: [
-        'channel-catalog',
-        'channel',
-        'channel:channel-1',
-        'user',
-        'user:user-1',
-      ],
-    };
+      const expectedTags = {
+        rawTags: [
+          'channel-catalog',
+          'channel',
+          'channel:channel-1',
+          'user',
+          'user:user-1',
+        ],
+      };
 
-    const expectedEvent = {
-      type: 'ChannelProfileEdited',
-      payload: {
-        channelId: 'channel-1',
-        editedBy: 'user-1',
-        name: 'online marketing',
-        description: 'all about online marketing',
-      },
-    };
+      const expectedEvent = {
+        type: 'ChannelProfileEdited',
+        payload: {
+          channelId: 'channel-1',
+          editedBy: 'user-1',
+          name: 'online marketing',
+          description: 'all about online marketing',
+        },
+      };
 
-    expect(result).toMatchObject([expectedTags, expectedEvent]);
+      expect(result).toMatchObject([expectedTags, expectedEvent]);
+    });
   });
-});
 
-describe('getChannelArchived', () => {
-  it('should return valid tags and event', () => {
-    const result = getChannelArchived('channel-1', 'user-1');
+  describe('getChannelArchived', () => {
+    it('should return valid tags and event', () => {
+      const result = getChannelArchived('channel-1', 'user-1');
 
-    const expectedTags = {
-      rawTags: [
-        'channel-catalog',
-        'channel',
-        'channel:channel-1',
-        'user',
-        'user:user-1',
-      ],
-    };
+      const expectedTags = {
+        rawTags: [
+          'channel-catalog',
+          'channel',
+          'channel:channel-1',
+          'user',
+          'user:user-1',
+        ],
+      };
 
-    const expectedEvent = {
-      type: 'ChannelArchived',
-      payload: {
-        channelId: 'channel-1',
-        archivedBy: 'user-1',
-      },
-    };
+      const expectedEvent = {
+        type: 'ChannelArchived',
+        payload: {
+          channelId: 'channel-1',
+          archivedBy: 'user-1',
+        },
+      };
 
-    expect(result).toMatchObject([expectedTags, expectedEvent]);
+      expect(result).toMatchObject([expectedTags, expectedEvent]);
+    });
   });
-});
 
-describe('getChannelUnarchived', () => {
-  it('should return valid tags and event', () => {
-    const result = getChannelUnarchived('channel-1', 'user-1');
+  describe('getChannelUnarchived', () => {
+    it('should return valid tags and event', () => {
+      const result = getChannelUnarchived('channel-1', 'user-1');
 
-    const expectedTags = {
-      rawTags: [
-        'channel-catalog',
-        'channel',
-        'channel:channel-1',
-        'user',
-        'user:user-1',
-      ],
-    };
+      const expectedTags = {
+        rawTags: [
+          'channel-catalog',
+          'channel',
+          'channel:channel-1',
+          'user',
+          'user:user-1',
+        ],
+      };
 
-    const expectedEvent = {
-      type: 'ChannelUnarchived',
-      payload: {
-        channelId: 'channel-1',
-        unarchivedBy: 'user-1',
-      },
-    };
+      const expectedEvent = {
+        type: 'ChannelUnarchived',
+        payload: {
+          channelId: 'channel-1',
+          unarchivedBy: 'user-1',
+        },
+      };
 
-    expect(result).toMatchObject([expectedTags, expectedEvent]);
+      expect(result).toMatchObject([expectedTags, expectedEvent]);
+    });
   });
-});
 
-describe('getChannelAssociatedUser', () => {
-  it('should return valid tags and event', () => {
-    const result = getChannelAssociatedUser('channel-1', 'user-1');
+  describe('getChannelAssociatedUser', () => {
+    it('should return valid tags and event', () => {
+      const result = getChannelAssociatedUser('channel-1', 'user-1');
 
-    const expectedTags = {
-      rawTags: [
-        'channel-catalog',
-        'channel',
-        'channel:channel-1',
-        'user',
-        'user:user-1',
-      ],
-    };
+      const expectedTags = {
+        rawTags: [
+          'channel-catalog',
+          'channel',
+          'channel:channel-1',
+          'user',
+          'user:user-1',
+        ],
+      };
 
-    const expectedEvent = {
-      type: 'ChannelAssociatedUser',
-      payload: {
-        channelId: 'channel-1',
-        userUUID: 'user-1',
-      },
-    };
+      const expectedEvent = {
+        type: 'ChannelAssociatedUser',
+        payload: {
+          channelId: 'channel-1',
+          userUUID: 'user-1',
+        },
+      };
 
-    expect(result).toMatchObject([expectedTags, expectedEvent]);
+      expect(result).toMatchObject([expectedTags, expectedEvent]);
+    });
   });
-});
 
-describe('getChannelDissociatedUser', () => {
-  it('should return valid tags and event', () => {
-    const result = getChannelDissociatedUser('channel-1', 'user-1');
+  describe('getChannelDissociatedUser', () => {
+    it('should return valid tags and event', () => {
+      const result = getChannelDissociatedUser('channel-1', 'user-1');
 
-    const expectedTags = {
-      rawTags: [
-        'channel-catalog',
-        'channel',
-        'channel:channel-1',
-        'user',
-        'user:user-1',
-      ],
-    };
+      const expectedTags = {
+        rawTags: [
+          'channel-catalog',
+          'channel',
+          'channel:channel-1',
+          'user',
+          'user:user-1',
+        ],
+      };
 
-    const expectedEvent = {
-      type: 'ChannelDissociatedUser',
-      payload: {
-        channelId: 'channel-1',
-        userUUID: 'user-1',
-      },
-    };
+      const expectedEvent = {
+        type: 'ChannelDissociatedUser',
+        payload: {
+          channelId: 'channel-1',
+          userUUID: 'user-1',
+        },
+      };
 
-    expect(result).toMatchObject([expectedTags, expectedEvent]);
+      expect(result).toMatchObject([expectedTags, expectedEvent]);
+    });
   });
 });
