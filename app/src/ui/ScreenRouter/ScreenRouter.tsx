0dcbffcc25d4717bb6070b12eb971a008634957c
GibboK <gibbok.coding@gmail.com>
2021-02-24 08:15:56 +0100

    fix wrong import

--- app/src/ui/ScreenRouter/ScreenRouter.tsx
+++ app/src/ui/ScreenRouter/ScreenRouter.tsx
@@ -4,7 +4,7 @@ import { AuthenticationContainer } from '../AuthenticationScreen/AuthenticationC
 import { StateContextUI } from '../ui-state-manager/UIStateManager';
 import { Screens } from '../ui-state-manager/types';
 import { ChatContainer } from '../ChatScreen/ChatContainer/ChatContainer';
-import { isSignedInUser } from '../../business-logic/channel-fish/logic';
+import { isSignedInUser } from '../../business-logic/user-catalog-fish/logic';
 
 type Props = Readonly<{
   pond: Pond;
