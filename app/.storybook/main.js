c4e7b24ff06953ccb8c9e650a6943f58a5e9638f
GibboK <gibbok.coding@gmail.com>
2021-02-25 12:32:07 +0100

    setup tailwind with storybook by switching to custom mode using webpack

--- app/.storybook/main.js
+++ app/.storybook/main.js
@@ -1,4 +1,25 @@
+const path = require('path');
+
 module.exports = {
+  webpackFinal: async (config, { configType }) => {
+    config.module.rules.push({
+      test: /\.css$/,
+      use: [
+        {
+          loader: 'postcss-loader',
+          options: {
+            ident: 'postcss',
+            plugins: [
+              require('tailwindcss'),
+              require('autoprefixer'),
+            ],
+          },
+        },
+      ],
+      include: path.resolve(__dirname, '../'),
+    });
+    return config;
+  },
   "stories": [
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
