66c255ab515baa376041b3587489b17a95dfa742
GibboK <gibbok.coding@gmail.com>
2021-02-25 16:22:55 +0100

    classname more readable

--- app/src/ui/common/Form/Submit/Submit.tsx
+++ app/src/ui/common/Form/Submit/Submit.tsx
@@ -9,11 +9,8 @@ export const Submit = ({ children, full = false }: SubmitProps) => {
   const styles = classnames(
     'font-sans',
     'bg-pink-900 hover:bg-pink-800',
-    'text-white text-xl',
-    'font-bold font-extrabold',
-    'rounded',
-    'h-11',
-    'p-2',
+    'text-white text-xl font-bold font-extrabold',
+    'rounded h-11 p-2',
     {
       'w-full': full,
     }
