683b5f1a1b13821c19533e5c49917122ed13d5cc
GibboK <gibbok.coding@gmail.com>
2021-03-01 14:45:04 +0100

    better button

--- app/src/ui/ChatScreen/Sidebar/Row.tsx
+++ app/src/ui/ChatScreen/Sidebar/Row.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from 'react';
+import { ButtonLink } from '../../common/ButtonLink/ButtonLink';
 
 export type RowProps = Readonly<{
   children: ReactNode;
@@ -7,11 +8,10 @@ export type RowProps = Readonly<{
 
 export const Row = ({ children, onClick }: RowProps) => {
   return (
-    <div
-      className="flex items-center space-x-2 w-full h-6 pl-2 pr-2 hover:bg-gray-600 cursor-pointer"
-      onClick={onClick}
-    >
-      {children}
+    <div className="w-full h-6 pl-2 pr-2 hover:bg-gray-600 cursor-pointer">
+      <ButtonLink click={onClick}>
+        <div className="flex items-center space-x-2">{children}</div>
+      </ButtonLink>
     </div>
   );
 };
