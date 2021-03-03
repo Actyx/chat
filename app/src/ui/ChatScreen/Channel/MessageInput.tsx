3b96bb08d7f3bd209bd9aef2dff4fb8dd3d670b5
GibboK <gibbok.coding@gmail.com>
2021-03-03 15:01:20 +0100

    align icon send

--- app/src/ui/ChatScreen/Channel/MessageInput.tsx
+++ app/src/ui/ChatScreen/Channel/MessageInput.tsx
@@ -25,17 +25,28 @@ export const MessageInput = ({ addMessage }: Props) => {
   return (
     <div className="h-24 p-4 w-full">
       <form onSubmit={handleSubmit}>
-        <textarea
-          rows={2}
-          className="rounded w-11/12"
-          value={message}
-          onChange={handleChangeContent}
-        />
-        <ButtonLink type="submit">
-          <div className="flex items-center justify-center bg-green-700 hover:bg-green-600 w-8 h-8 rounded transform rotate-90">
-            <PaperAirplane color="white" fill={true} />
+        <div className="flex space-x-2">
+          <textarea
+            rows={2}
+            className="rounded w-11/12"
+            value={message}
+            onChange={handleChangeContent}
+          />
+          <div className="flex self-end">
+            <ButtonLink type="submit">
+              <div className="flex items-center justify-center bg-green-700 hover:bg-green-600 w-8 h-8 rounded transform rotate-90">
+                <svg
+                  className="h-5 w-5 text-white"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z" />
+                </svg>
+              </div>
+            </ButtonLink>
           </div>
-        </ButtonLink>
+        </div>
       </form>
     </div>
   );
