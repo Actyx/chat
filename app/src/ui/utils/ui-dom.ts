92d5a3e19349959797b6563e93f8e89dcd2eea57
GibboK <gibbok.coding@gmail.com>
2021-03-04 18:24:38 +0100

    remove dom util

--- app/src/ui/utils/ui-dom.ts
+++
@@ -1,7 +0,0 @@
-export const scrollDomIntoView = (dataAttribute: string) => (
-  id: string
-): void => {
-  document.querySelector(`[${dataAttribute}="${id}"]`)?.scrollIntoView();
-  const dom = document.querySelector(`[${dataAttribute}="${id}"]`);
-  console.log(dom?.getBoundingClientRect());
-};
