c3eaf40522d03fa7f83a2c9595870ec484db64bb
GibboK <gibbok.coding@gmail.com>
2021-03-08 14:20:09 +0100

    add ErrorBoundary

---
+++ app/src/ui/common/ErrorBoundary/ErrorBoundary.tsx
@@ -0,0 +1,33 @@
+import React, { ReactNode, ErrorInfo } from 'react';
+
+interface Props {
+  children: ReactNode;
+}
+
+interface State {
+  hasError: boolean;
+}
+
+export class ErrorBoundary extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error: Error) {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    console.error(error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+      return <h1>Something went wrong.</h1>;
+    }
+
+    return this.props.children;
+  }
+}
