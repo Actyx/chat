b60e72a0ecd3e863dd01aadc988e4e5a7cdbd07f
GibboK <gibbok.coding@gmail.com>
2021-03-11 13:17:09 +0100

    wrap in promise

--- app/src/business-logic/user-catalog-fish/wire.tsx
+++ app/src/business-logic/user-catalog-fish/wire.tsx
@@ -12,19 +12,25 @@ import { UserCatalogFish } from './user-catalog-fish';
 export const signUpWire = (pond: Pond, makerUUID: () => UserUUID) => async (
   displayName: string,
   email: Email
-): Promise<SignUpLogicResult> => {
-  let resultOutput: SignUpLogicResult;
-  await pond
-    .run<UserCatalogFishState, UserCatalogFishEvent>(
-      UserCatalogFish,
-      (fishState, enqueue) => {
-        const result = signUpLogic(makerUUID, fishState)(displayName, email);
-        if (result.status === 'ok') {
-          enqueue(...result.tagsWithEvents[0]);
-        }
-        resultOutput = result;
-      }
-    )
-    .toPromise();
-  return resultOutput!;
-};
+): Promise<SignUpLogicResult> =>
+  new Promise(async (res, rej) => {
+    try {
+      await pond
+        .run<UserCatalogFishState, UserCatalogFishEvent>(
+          UserCatalogFish,
+          (fishState, enqueue) => {
+            const result = signUpLogic(makerUUID, fishState)(
+              displayName,
+              email
+            );
+            if (result.status === 'ok') {
+              enqueue(...result.tagsWithEvents[0]);
+            }
+            res(result);
+          }
+        )
+        .toPromise();
+    } catch (err) {
+      rej(err);
+    }
+  });
